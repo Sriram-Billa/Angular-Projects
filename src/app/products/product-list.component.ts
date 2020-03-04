6a9bbcfe1e315ecbf09211e065f5bd856b233773
Sriram-Billa <58298040+Sriram-Billa@users.noreply.github.com>
2020-03-04 23:12:46 +0530

    all files updated-after property binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
 })
 
 export class productListComponent {
-    pageTitle:string = 'Product List Page'    
+    pageTitle:string = 'Product List Page';
+    imageWidth:number = 100;
+    imageMargin:number = 10;
+    products:any[] = [{
+
+        "productId":1,
+        "productName":"Lock",
+        "productCode":"Lock-1",
+        "availability":"March 12,2020",
+        "price":20.0,
+        "description":"Lock image",
+        "starRating":3.5,
+        "imageUrl":"http://clipart-library.com/images/5iRXR7gRT.jpg"
+    },{
+    "productId":2,
+    "productName":"Penguin",
+    "productCode":"Penguin-1",
+    "availability":"March 1,2020",
+    "price":24.0,
+    "description":"Penguin image",
+    "starRating":4.4,
+    "imageUrl":"http://clipart-library.com/images/BcaE5KEXi.png"
+    }
+]
+
 }
