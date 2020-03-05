b665fe21dcb6cdc42752954b8093c94d7ccc1eca
Sriram-Billa <58298040+Sriram-Billa@users.noreply.github.com>
2020-03-05 13:56:50 +0530

    third video done

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -6,29 +7,37 @@ import { Component } from '@angular/core';
 })
 
 export class productListComponent {
-    pageTitle:string = 'Product List Page';
-    imageWidth:number = 100;
-    imageMargin:number = 10;
-    products:any[] = [{
+    pageTitle: string = 'Product List Page';
+    imageWidth: number = 100;
+    imageMargin: number = 10;
+    showImage: boolean = false;
+    listFilter:string = "Sriram";
+    products: IProduct[] = [{
 
-        "productId":1,
-        "productName":"Lock",
-        "productCode":"Lock-1",
-        "availability":"March 12,2020",
-        "price":20.0,
-        "description":"Lock image",
-        "starRating":3.5,
-        "imageUrl":"http://clipart-library.com/images/5iRXR7gRT.jpg"
-    },{
-    "productId":2,
-    "productName":"Penguin",
-    "productCode":"Penguin-1",
-    "availability":"March 1,2020",
-    "price":24.0,
-    "description":"Penguin image",
-    "starRating":4.4,
-    "imageUrl":"http://clipart-library.com/images/BcaE5KEXi.png"
+        "productId": 1,
+        "productName": "Lock",
+        "productCode": "Lock-1",
+        "availability": "March 12,2020",
+        "price": 20.0,
+        "description": "Lock image",
+        "starRating": 3.5,
+        "imageUrl": "http://clipart-library.com/images/5iRXR7gRT.jpg"
+    }, {
+        "productId": 2,
+        "productName": "Penguin",
+        "productCode": "Penguin-1",
+        "availability": "March 1,2020",
+        "price": 24.0,
+        "description": "Penguin image",
+        "starRating": 4.4,
+        "imageUrl": "http://clipart-library.com/images/BcaE5KEXi.png"
     }
-]
+    ]
+
+    toggleImage(): void {
+
+        this.showImage = !this.showImage;
+
+    }
 
 }
